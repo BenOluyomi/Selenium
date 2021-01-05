2eb7858efee7425dd399cacb4341dbd1bf79e092
benjamin.oluyomi <boluyomi@academytrainee.com>
2021-01-05 11:39:47 +0000

    FTSE - working

--- FTSEWebsite/src/test/java/FTSE.java
+++ FTSEWebsite/src/test/java/FTSE.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FTSE {
@@ -13,17 +14,23 @@ public class FTSE {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe"); // set driver path
 		driver = new ChromeDriver();
 
-		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100"); // 
-
-		//WebElement riser = driver.findElement(By.xpath("//a[@href='https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers']"));
-		//riser.click();
-		WebDriverWait wait = new WebDriverWait (driver, 3000);
-		 WebElement p=driver.findElement(By.partialLinkText("RISERS"));
-		 
-	     p.click();
-		
+		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers"); //
 		String innerText = driver.findElement(By.xpath("//*[@id=\"ls-row-NXT-L\"]/td[1]")).getText();
 		System.out.println(innerText);
+		
+		
+		// WebElement riser =
+		// driver.findElement(By.xpath("//a[@href='https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers']"));
+		// riser.click();
+
+		//WebElement p = driver.findElement(By.partialLinkText("RISERS"));
+		// cant click link with either method?
+		//p.click();
+		
+		driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100/fallers"); //
+		String innerText1 = driver.findElement(By.xpath("//*[@id=\"ls-row-ABF-L\"]/td[1]")).getText();
+		System.out.println(innerText1);
+		
 
 	}
 
